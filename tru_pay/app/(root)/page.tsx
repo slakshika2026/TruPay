5ca12faad455504b9329edc08144d11a5a47780d
Sanduni Lakshika <110313255+slakshika2026@users.noreply.github.com>
2024-09-12 10:15:53 +0530

    feat: total balance displayed

--- tru_pay/app/(root)/page.tsx
+++ tru_pay/app/(root)/page.tsx
@@ -1,11 +1,29 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>
-      HOME
-    </div>
-  )
+   const loggedIn = { firstName: 'Sanduni' };
+   return (
+      <section className='home'>
+         <div className="home-content">
+            <header className="home-header">
+               <HeaderBox
+                  type="greeting"
+                  title="Welcome"
+                  user={loggedIn?.firstName || 'Guest'}
+                  subtext="Access and manage your transactions"
+               />
+               <TotalBalanceBox
+                  accounts={[]}
+                  totalBanks={1}
+                  totalCurrentBalance={ 1250.30}
+               />
+            </header>
+         </div>
+
+      </section>
+   )
 }
 
 export default Home
